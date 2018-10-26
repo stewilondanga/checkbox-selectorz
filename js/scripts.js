38432cdcac1bbaca6b50277cf62b6129a295e7d9
Stewart ilondanga <stilondanga@gmail.com>
2018-10-26 15:16:26 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -11,11 +11,11 @@ $(".dropdown dt a").on('click', function() {
   $(".dropdown dd ul").slideToggle('fast');
 });
 
-/*$(".dropdown dd ul li a").on('click', function() {
+$(".dropdown dd ul li a").on('click', function() {
   $(".dropdown dd ul").hide();
 });
 
-function getSelectedValue(id) {
+/*function getSelectedValue(id) {
   return $("#" + id).find("dt a span.value").html();
 }
 
